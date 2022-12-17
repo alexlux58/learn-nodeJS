fa4467edc4da5b17c31f79522cbc9cfd0169c949
Alex Lux <alexlux@live.com>
2022-12-16 22:37:41 -0800

    added routing for urls and started bulding a simple API

--- 1-node-farm/starter/index.js
+++ 1-node-farm/starter/index.js
@@ -21,10 +21,33 @@ const fs = require("fs");
 
 // SERVER
 const http = require("http");
+const url = require("url");
 
 const server = http.createServer((req, res) => {
-  console.log(req);
-  res.end("Hello from the server");
+  // console.log(req);
+  // console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === "/overview") {
+    res.end("Hello from the overview");
+  } else if (pathName === "/product") {
+    res.end("Hello from the product");
+  } else if (pathName === "/api") {
+    fs.readFile(`${__dirname}/dev-data/data.json`, "utf-8", (err, data) => {
+      const productData = JSON.parse(data);
+      console.log(productData);
+      res.writeHead(200, { "Content-type": "application/json" });
+      res.end(data);
+    });
+
+    res.end("API");
+  } else {
+    res.writeHead(404, {
+      "Content-type": "text/html",
+      "my-own-header": "alex-lux",
+    });
+    res.end("<h1>404 Page not found</h1>");
+  }
 });
 
 server.listen(8000, "127.0.0.1", () => {
